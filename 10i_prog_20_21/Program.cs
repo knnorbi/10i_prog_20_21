5d9ce986ff16e230772316d9e52d021dcf11c077
knnorbi <kluka.norbert@gmail.com>
2020-12-07 08:22:07 +0100

    20201207

--- 10i_prog_20_21/Program.cs
+++ 10i_prog_20_21/Program.cs
@@ -13,7 +13,7 @@ namespace _10i_prog_20_21
             int ertek = 0;
             for (int i = 0; i < x.Length; i++)
             {
-                ertek += x[i];                   // ertek = ertek + x[i];
+                ertek += x[i];                      // ertek = ertek + x[i];
             }
             return ertek;
         }
@@ -28,15 +28,67 @@ namespace _10i_prog_20_21
             return i < x.Length;
         }
 
+        static int Kivalasztas(int[] x)
+        {
+            int i = 0;
+            while (!(x[i] % 2 == 0))
+            {
+                i++;
+            }
+            return i;
+        }
+
+        static int LinearisKereses(int[] x)
+        {
+            int i = 0;
+            while (i < x.Length && !(x[i] > 10))
+            {
+                i++;
+            }
+            if (i < x.Length)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static int Megszamlalas(int[] x)
+        {
+            int db = 0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] % 3 == 0)
+                {
+                    db++;
+                }
+            }
+
+            return db;
+        }
+
         static void Main(string[] args)
         {
-            int[] t = { 42, 6, 7, 65, 8, 54, 23, 45, 9, 10 };
+            int[] t = { 41, 7, 6, 65, 8, 54, 23, 45, 9, 10 };
 
             int sorozat = Sorozatszamitas(t);
             Console.WriteLine(sorozat);
 
             bool vane = Eldontes(t);
             Console.WriteLine(vane);
+
+            int elsoparos = Kivalasztas(t);
+            Console.WriteLine(elsoparos);
+
+            int elsotiznelnagyobb = LinearisKereses(t);
+            Console.WriteLine(elsotiznelnagyobb);
+
+            int harommaloszthatoak = Megszamlalas(t);
+            Console.WriteLine(harommaloszthatoak);
+
 
             Console.ReadKey();
         }
